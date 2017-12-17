df0e3a3f7a37108c13b551b1a63fe3163299121e
Nadir SEHNOUN <nsehnoun@e1r1p10.42.fr>
2017-12-17 14:04:42 +0100

    ptite mod

--- src/app/mock-experience.ts
+++ src/app/mock-experience.ts
@@ -1,6 +1,6 @@
 import {Experience} from './experience';
 
-export const EXPERIENCES: Experience = [
+export const EXPERIENCES: Experience[] = [
   {id: 1, name: "Full stack Web Developer", company:"L'atelier des Chefs", url: "adc.png"},
   {id: 2, name: "Software Developer", company: "Bull Atos", url: "bullatos.png"},
   {id: 3, name: "Software Developer", company: "France Média Monde", url: "fmm.png"}
