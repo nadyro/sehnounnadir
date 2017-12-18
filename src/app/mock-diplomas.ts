dcf84b257542cce986d0b2ee594c84a8951c268a
Nadir SEHNOUN <nsehnoun@e3r1p9.42.fr>
2017-12-18 17:54:44 +0100

    progress

--- src/app/mock-diplomas.ts
+++ src/app/mock-diplomas.ts
@@ -1,7 +1,7 @@
 import {Diploma} from './diplomas';
 
 export const DIPLOMAS: Diploma[] = [
-    {id: 1, name: "IT Technician", place: "Lycée Turgot"},
-    {id: 2, name: "Bachelor in Web Development", place: "ESGI"},
-    {id: 3, name: "Technology Architect", place: "42"}
+  {id: 3, name: "Technology Architect", place: "42"},
+  {id: 2, name: "Bachelor in Web Development", place: "ESGI"},
+  {id: 1, name: "IT Technician", place: "Lycée Turgot"}  
 ]
